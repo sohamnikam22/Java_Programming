20a987f23ed21ebb0fb626826ad807e2b8d258cb
sohamnikam22 <sohamnikam2202@gmail.com>
2026-04-24 20:25:08 +0530

    Add files via upload

---
+++ DivisionX.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class DivisionX
+{
+
+    public static void main(String A[])
+    {
+       Scanner sobj = new Scanner(System.in);
+
+       int No1 = 0, No2 = 0, Ans = 0;
+
+       System.out.println("Enter First number");
+       No1 = sobj.nextInt();
+
+       System.out.println("Enter Second number");
+       No2 = sobj.nextInt();
+
+       Ans = No1 / No2;
+
+       System.out.println("Division is : "+Ans);
+       
+    }
+}
