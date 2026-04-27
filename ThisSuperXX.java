206ced567788ade8c3039e0203874d19ad367c7f
sohamnikam22 <sohamnikam2202@gmail.com>
2026-04-27 22:14:01 +0530

    Add files via upload

---
+++ ThisSuperXX.java
@@ -0,0 +1,62 @@
+class Base
+{
+   public int i;
+   
+   public Base()
+   {
+     System.out.println("Inside Base constructor");
+     this.i = 11;
+     
+   }
+
+   
+    
+   public void fun()
+   {
+     System.out.println("Inside Base fun");
+
+   }
+}
+
+class Derived extends Base
+{
+   public int i;
+   
+   public Derived()
+   {
+     System.out.println("Inside Derived constructor");
+
+     this.i = 21;
+
+   }
+
+   
+
+   public void gun()
+   {
+      System.out.println("Inside gun of derived");
+      System.out.println("Value of i : "+i);
+      System.out.println("Value of i from base : "+super.i);
+      
+
+   }
+
+}
+
+
+
+class ThisSuperXX
+{
+    public static void main(String A[])
+    {
+       Derived dobj = new Derived();
+
+       dobj.gun();
+
+       
+
+    }
+  
+
+
+}
