99c8ddff9ff2d4904cf7ffb7426b7ee38419eb70
sohamnikam22 <sohamnikam2202@gmail.com>
2026-04-28 12:39:14 +0530

    Add files via upload

---
+++ SingleGC.java
@@ -0,0 +1,83 @@
+class Base
+{
+   public int i;
+   public int j;
+
+   public Base()
+   {
+     System.out.println("Inside Base constructor");
+     this.i = 0;
+     this.j = 0;
+
+   }
+
+   protected void finalize()
+   {
+      System.out.println("Inside finalize method of Base");
+
+   } 
+    
+   public void fun()
+   {
+     System.out.println("Inside Base fun");
+
+   }
+}
+
+class Derived extends Base
+{
+   public int x;
+   
+   public Derived()
+   {
+     System.out.println("Inside Derived constructor");
+
+     this.x = 0;
+
+   }
+
+   protected void finalize()
+   {
+      System.out.println("Inside finalize method of Base");
+
+   }
+
+   public void gun()
+   {
+      System.out.println("Inside gun of derived");
+
+   }
+
+}
+
+
+
+class SingleGC
+{
+    public static void main(String A[])
+    {
+       
+       System.out.println("Inside Main");
+
+       Derived dobj = new Derived();
+
+       System.out.println(dobj.i);
+       System.out.println(dobj.j);
+       System.out.println(dobj.x);
+
+       dobj.fun();
+       dobj.gun();
+
+       dobj = null;
+
+       System.gc();
+
+
+
+       System.out.println("End of Main");
+
+    }
+  
+
+
+}
