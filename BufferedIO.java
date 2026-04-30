c16e771db25ae3874e5006e33c5ffd0531b18009
sohamnikam22 <sohamnikam2202@gmail.com>
2026-04-30 22:02:06 +0530

    Add files via upload

---
+++ BufferedIO.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class BufferedIO
+{
+    public static void main(String A[])
+    {
+       BufferedReader bobj = new BufferedReader(new InputStream Reader(System.in));
+
+       system.out.println("Enter your name : "):
+       String name = bobj.readline();
+
+       System.out.println("Hello "+name); 
+
+    }
+}
+
