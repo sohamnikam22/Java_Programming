bb1810ee87b578018767e52d4559cb4e7dc8426b
sohamnikam22 <sohamnikam2202@gmail.com>
2026-05-05 22:44:17 +0530

    Add files via upload

---
+++ MainThread.java
@@ -0,0 +1,15 @@
+class MainThread
+{
+
+    public static void main(String A[])
+    {
+        System.out.println("Inside main Method");
+
+        Thread tobj = Thread.currentThread();
+
+        String name = tobj.getName();
+
+        System.out.println("Name of current thread :"+name);
+
+    }
+}
