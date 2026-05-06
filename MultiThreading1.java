fb69ff98f5cb49d2d9408a84f119859bbb1713c6
sohamnikam22 <sohamnikam2202@gmail.com>
2026-05-06 22:12:27 +0530

    Add files via upload

---
+++ MultiThreading1.java
@@ -0,0 +1,24 @@
+class demo extends Thread
+{
+   public void run()
+   {
+    System.out.println("Inside run method");
+   }
+}
+
+class MultiThreading1
+{
+
+    public static void main(String A[])
+    {
+       System.out.println("Main thread is running...");
+
+       Demo dobj1 = new Demo();
+       Demo dobj2 = new Demo();
+
+       dobj1.start();
+       dobj2.start();
+       
+       
+    }
+}
