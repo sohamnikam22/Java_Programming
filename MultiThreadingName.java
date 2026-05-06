933828ec9ede53153984339a34fe5637efa4cca8
sohamnikam22 <sohamnikam2202@gmail.com>
2026-05-06 22:13:43 +0530

    Add files via upload

---
+++ MultiThreadingName.java
@@ -0,0 +1,27 @@
+class demo extends Thread
+{
+   public void run()
+   {
+    System.out.println("Inside run method of :"+Thread.currentThread().getName());
+   }
+}
+
+class MultiThreadingName
+{
+
+    public static void main(String A[])
+    {
+       System.out.println("Main thread is running...");
+
+       Demo dobj1 = new Demo();
+       Demo dobj2 = new Demo();
+       
+       dobj1.setName("First");
+       dobj2.setName("Second");
+
+       dobj1.start();
+       dobj2.start();
+       
+       
+    }
+}
